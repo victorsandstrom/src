2668cc7d48e438ae4f05fb3b268733d25758cc5c
victorsandstrom <victorsandstrom91@hotmail.com>
2014-05-22 15:29:28 +0200

    The Node Class

--- sensorynetwork/Node.java
+++ sensorynetwork/Node.java
@@ -1,6 +1,30 @@
 package sensorynetwork;
+import java.util.*;
+
 
 
 public class Node {
 
+	
+	/*
+	 * Två attribut för att hantera meddelanden under varje timeTick
+	 * TODO Skapa en klass "Request" för att representera meddelanden
+	 */
+	
+	Request activeMessage = new Request();
+	LinkedList messageQueue = new LinkedList(); 
+	
+	
+	
+	/* Funktion: Move()
+	 * 1. Meddelande tas emot, meddelande sätts in i activeMessage 
+	 * 2. Om activeMessage är "null" så stoppas nya meddelanden in i kön messageQueue eller
+	 * så skickas det aktiva meddelandet vidare
+	 */
+	private void Move(){
+		
+	}
+	
+	
+	
 }
