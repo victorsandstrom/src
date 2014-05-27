84d9e8e17f68274af2451d7701cb05d1c77d647e
zarsvictor <zarsvictor@users.noreply.github.com>
2014-05-27 14:22:18 +0200

    Update Request.java

--- sensorynetwork/Request.java
+++ sensorynetwork/Request.java
@@ -1,8 +1,33 @@
-package sensorynetwork;
 
-public class Request {
+public class Request extends Agent {
 
-	public Request(){
-		
+	private int eventId;
+	private boolean foundWay = false;
+
+	Request(Node node, int id){
+		super(node);
+		eventId = id;
+		//node.messageRequest(eventId);
 	}
+
+	public void move(){
+		if(foundWay){
+			path.push(currentNode);
+			currentNode = currentNode.getHashmap().get(eventId);
+			pos = currentNode.getPos();
+		}
+		else{
+			super.move();
+		}
+		if (currentNode.request(this)){
+
+		}
+	}
+
+	private void updateInfo(){
+		if()
+	}
+
+
+
 }
